83fe7ad6d3151559c4c0cdcd99e256f1363d4966
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-25 21:43:26 +0800

    Revert "Revert "Add animated volume icon when playing media on the device""
    This reverts commit 873dfb5ff6df1a3b82ef8efc7c3f316a82099790.
    
    Change-Id: I582f92ad08558a907320a8bd5b65b94c3bfa3ff7

--- packages/SystemUI/src/com/android/systemui/volume/dagger/VolumeModule.java
+++ packages/SystemUI/src/com/android/systemui/volume/dagger/VolumeModule.java
@@ -38,12 +38,14 @@ import com.android.systemui.volume.CsdWarningDialog;
 import com.android.systemui.volume.VolumeComponent;
 import com.android.systemui.volume.VolumeDialogComponent;
 import com.android.systemui.volume.VolumeDialogImpl;
+import com.android.systemui.volume.VolumeDialogModule;
 import com.android.systemui.volume.VolumePanelDialogReceiver;
 import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.volume.domain.interactor.VolumeDialogInteractor;
 import com.android.systemui.volume.domain.interactor.VolumePanelNavigationInteractor;
 import com.android.systemui.volume.panel.dagger.VolumePanelComponent;
 import com.android.systemui.volume.panel.dagger.factory.VolumePanelComponentFactory;
+import com.android.systemui.volume.ui.binder.VolumeDialogMenuIconBinder;
 import com.android.systemui.volume.ui.navigation.VolumeNavigator;
 
 import dagger.Binds;
@@ -63,6 +65,7 @@ import dagger.multibindings.IntoSet;
                 CaptioningModule.class,
                 MediaDevicesModule.class,
                 SpatializerModule.class,
+                VolumeDialogModule.class,
         },
         subcomponents = {
                 VolumePanelComponent.class
@@ -114,6 +117,7 @@ public interface VolumeModule {
             DumpManager dumpManager,
             Lazy<SecureSettings> secureSettings,
             VibratorHelper vibratorHelper,
+            VolumeDialogMenuIconBinder volumeDialogMenuIconBinder,
             SystemClock systemClock,
             VolumeDialogInteractor interactor) {
         VolumeDialogImpl impl = new VolumeDialogImpl(
@@ -133,6 +137,7 @@ public interface VolumeModule {
                 dumpManager,
                 secureSettings,
                 vibratorHelper,
+                volumeDialogMenuIconBinder,
                 systemClock,
                 interactor);
         impl.setStreamImportant(AudioManager.STREAM_SYSTEM, false);
