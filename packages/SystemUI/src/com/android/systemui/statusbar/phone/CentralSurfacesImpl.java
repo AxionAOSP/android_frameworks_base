7c8b33d2bd6cd4dc6a73bb0c794cb865fc95ab93
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-07 09:33:47 +0800

    SystemUI: Introduce ScrimUtils
    Change-Id: Iedc76947b781c9958ea68e5f34398500ffa15b8c
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2675,6 +2675,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            com.android.systemui.util.ScrimUtils.getInstance(mContext).onScreenStateChange();
         }
 
         @Override
