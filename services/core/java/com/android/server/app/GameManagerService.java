691fe6ef6fbcc58afe06dfecdea3024babcb6be8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-07 16:00:42 +0800

    GameManagerService: Add performance mode hooks if application config is set to performance, try to tweak the scheduler as well to improve app performance
    test:
    adb shell getprop | grep persist.sys.power_mode_perf
    adb shell settings get system persist.sys.power_mode_perf
    adb shell cat /proc/sys/kernel/sched_migration_cost_ns
    
    Change-Id: Ie2aa5ee544f679dc91bbb854d1716e88e77eac97
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -156,6 +156,8 @@ public final class GameManagerService extends IGameManagerService.Stub {
             "debug.graphics.game_default_frame_rate.disabled";
     static final String PROPERTY_RO_SURFACEFLINGER_GAME_DEFAULT_FRAME_RATE =
             "ro.surface_flinger.game_default_frame_rate_override";
+    static final String PROPERTY_PERSIST_PERFORMANCE_MODE = 
+            "persist.sys.power_mode_perf";
 
     private static final String PACKAGE_NAME_MSG_KEY = "packageName";
     private static final String USER_ID_MSG_KEY = "userId";
@@ -1586,6 +1588,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
         mPowerManagerInternal.setPowerMode(Mode.GAME_LOADING, false);
         Slog.v(TAG, "Game power mode OFF (game manager service start/restart)");
         mPowerManagerInternal.setPowerMode(Mode.GAME, false);
+        boostGameService(false);
 
         mGameDefaultFrameRateValue = (float) mSysProps.getInt(
                 PROPERTY_RO_SURFACEFLINGER_GAME_DEFAULT_FRAME_RATE, 60);
@@ -2328,6 +2331,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
                         if (!mGameForegroundUids.isEmpty() && mNonGameForegroundUids.isEmpty()) {
                             Slog.v(TAG, "Game power mode OFF (first non-game in foreground)");
                             mPowerManagerInternal.setPowerMode(Mode.GAME, false);
+                            boostGameService(false);
                         }
                         mNonGameForegroundUids.add(uid);
                     }
@@ -2337,6 +2341,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
                         || mNonGameForegroundUids.isEmpty())) {
                     Slog.v(TAG, "Game power mode ON (first game in foreground)");
                     mPowerManagerInternal.setPowerMode(Mode.GAME, true);
+                    boostGameService(true);
                 }
                 final boolean isGameDefaultFrameRateDisabled =
                         mSysProps.getBoolean(
@@ -2355,12 +2360,14 @@ public final class GameManagerService extends IGameManagerService.Stub {
                             || mNonGameForegroundUids.isEmpty())) {
                         Slog.v(TAG, "Game power mode OFF (no games in foreground)");
                         mPowerManagerInternal.setPowerMode(Mode.GAME, false);
+                        boostGameService(false);
                     }
                 } else if (disableGameModeWhenAppTop() && mNonGameForegroundUids.contains(uid)) {
                     mNonGameForegroundUids.remove(uid);
                     if (mNonGameForegroundUids.isEmpty() && !mGameForegroundUids.isEmpty()) {
                         Slog.v(TAG, "Game power mode ON (only games in foreground)");
                         mPowerManagerInternal.setPowerMode(Mode.GAME, true);
+                        boostGameService(true);
                     }
                 }
             }
@@ -2406,4 +2413,14 @@ public final class GameManagerService extends IGameManagerService.Stub {
         }
     }
 
+    void boostGameService(boolean enable) {
+        boolean perfModeEnabledByUser = Settings.System.getIntForUser(
+            mContext.getContentResolver(), "power_mode_perf_by_user", 0,
+            UserHandle.USER_CURRENT) == 1;
+        if (perfModeEnabledByUser) return;
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+            PROPERTY_PERSIST_PERFORMANCE_MODE, enable ? 1 : 0,
+            UserHandle.USER_CURRENT);
+        SystemProperties.set(PROPERTY_PERSIST_PERFORMANCE_MODE, enable ? "1" : "0");
+    }
 }
