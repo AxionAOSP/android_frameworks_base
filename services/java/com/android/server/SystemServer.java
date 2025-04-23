1936f6cdcd73444b04e4100666cc14b0a2b2f832
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-24 06:47:29 +0800

    services: improve lock recents task feature
    Change-Id: Ie2636a1038d59e086b34dc569d9b1edacd97ac26
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2818,6 +2818,7 @@ public final class SystemServer implements Dumpable {
             
             mSystemServiceManager.startService(HideAppListService.class);
             mSystemServiceManager.startService(GameSpaceManagerService.class);
+            mSystemServiceManager.startService(PinnedTaskService.class);
         }
 
         t.traceBegin("StartMediaProjectionManager");
