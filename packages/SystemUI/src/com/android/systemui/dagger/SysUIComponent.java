05df1518ac59292b924c7d9255197a82d966aa3d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 14:55:08 +0800

    [feature] adding wifi standard icon
    icons from: a799a35c5527482f2d577783170b0478b8021b3f "base: Refactor Wi-Fi standard icons"
    
    rmp - rewrite from scratch - use MVVM - avoid memory leaks - add support for multiple instances (QS->StatusBar)
    
    Change-Id: I187fc97b9f4ad6d48b6c6f0b1215ffbcb1c972e2
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -35,6 +35,8 @@ import com.android.systemui.statusbar.QsFrameTranslateModule;
 import com.android.systemui.statusbar.phone.ConfigurationForwarder;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.theme.UiStyleProvider;
+import com.android.systemui.statusbar.WifiStandardRepository;
+import com.android.systemui.statusbar.WifiStandardViewController;
 import com.android.wm.shell.appzoomout.AppZoomOut;
 import com.android.wm.shell.back.BackAnimation;
 import com.android.wm.shell.bubbles.Bubbles;
@@ -199,6 +201,12 @@ public interface SysUIComponent {
      * Returns {@link CoreStartable} dependencies if there are any.
      */
     @Dependencies Map<Class<?>, Set<Class<? extends CoreStartable>>> getStartableDependencies();
+    
+    @SysUISingleton
+    WifiStandardRepository wifiStandardRepository();
+
+    @SysUISingleton
+    WifiStandardViewController.Factory wifiStandardViewControllerFactory();
 
     @SysUISingleton
     DozeScreenStateEx dozeScreenStateEx();
