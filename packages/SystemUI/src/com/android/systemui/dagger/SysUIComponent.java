796ce2fc98bef957f2150209491b374941f01792
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-07 11:59:23 +0800

    [Feature] add option to disable quicksettings on keyguard
    reverse-engineered from nothingOS - simplified
    
    Change-Id: Id2910cdc4e753609be3cbe7817605e27b5f6a53f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -30,6 +30,7 @@ import com.android.systemui.media.MediaViewController;
 import com.android.systemui.people.PeopleProvider;
 import com.android.systemui.startable.Dependencies;
 import com.android.systemui.statusbar.NotificationInsetsModule;
+import com.android.systemui.statusbar.NTForbiddenSwipeDownQSController;
 import com.android.systemui.statusbar.QsFrameTranslateModule;
 import com.android.systemui.statusbar.phone.ConfigurationForwarder;
 import com.android.systemui.statusbar.policy.ConfigurationController;
@@ -194,6 +195,9 @@ public interface SysUIComponent {
 
     @SysUISingleton
     MistouchPreventionWindowController mistouchPreventionWindowController();
+
+    @SysUISingleton
+    NTForbiddenSwipeDownQSController forbiddenSwipeDownQSController();
 
     /**
      * Member injection into the supplied argument.
