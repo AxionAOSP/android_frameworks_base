3961fdbc3f92395e5a581de35ee2520e41a51bd2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-25 16:05:03 +0800

    [boost_fwk] adding memory manager
    Change-Id: Icbcd21db7ee04a8d6a755648fa9df71dc82ce52a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -243,6 +243,7 @@ import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.server.AccessibilityManagerInternal;
+import com.android.server.AxExtServiceFactory;
 import com.android.server.DockObserverInternal;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
@@ -6047,6 +6048,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return;
         }
         EventLogTags.writeScreenToggled(1);
+        
+        AxExtServiceFactory.getMemoryManager().releaseMemoryAtScreenOn();
+        AxExtServiceFactory.getMemoryManager().loadProcessMemory("com.android.systemui");
+        AxExtServiceFactory.getMemoryManager().loadProcessMemory("com.android.launcher3");
 
         mIsGoingToSleepDefaultDisplay = false;
         mDefaultDisplayPolicy.setAwake(true);
