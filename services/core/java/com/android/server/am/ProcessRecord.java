79bebd6386aeebcf47439246565adc6210c2d1b9
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-25 18:07:42 +0800

    [services] adding axion system server hooks
    Change-Id: Ie2732937474a9daceddea6d6365ea67c7bab9e41

--- services/core/java/com/android/server/am/ProcessRecord.java
+++ services/core/java/com/android/server/am/ProcessRecord.java
@@ -1761,4 +1761,20 @@ class ProcessRecord implements WindowProcessListener {
             consumer.accept(provider);
         }
     }
+    
+    public ProcessStateRecord getState() {
+        return this.mState;
+    }
+
+    public String getProcessName() {
+        return this.processName;
+    }
+
+    public int getUid() {
+        return this.uid;
+    }
+
+    public int getCurAdj() {
+        return this.mState.getCurAdj();
+    }
 }
