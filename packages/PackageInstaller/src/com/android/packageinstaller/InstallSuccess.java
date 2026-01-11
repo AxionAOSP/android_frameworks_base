b291239feb3f305365459d79651b0001d4192853
Mohamed Elsayed <mohamed20020093@gmail.com>
2026-01-11 20:17:40 +0200

    Refactor InstallSuccess for better dialog management
    Refactor InstallSuccess activity to improve dialog handling and code clarity.

--- packages/PackageInstaller/src/com/android/packageinstaller/InstallSuccess.java
+++ packages/PackageInstaller/src/com/android/packageinstaller/InstallSuccess.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package com.android.packageinstaller;
 
 import android.app.Activity;
@@ -33,9 +17,6 @@ import androidx.annotation.Nullable;
 
 import java.util.List;
 
-/**
- * Finish installation: Return status code to the caller or display "success" UI to user
- */
 public class InstallSuccess extends Activity {
     private static final String LOG_TAG = InstallSuccess.class.getSimpleName();
 
@@ -48,6 +29,7 @@ public class InstallSuccess extends Activity {
     @Nullable
     private Intent mLaunchIntent;
 
+    @Nullable
     private AlertDialog mDialog;
 
     @Override
@@ -57,23 +39,29 @@ public class InstallSuccess extends Activity {
         setFinishOnTouchOutside(true);
 
         if (getIntent().getBooleanExtra(Intent.EXTRA_RETURN_RESULT, false)) {
-            // Return result if requested
             Intent result = new Intent();
             result.putExtra(Intent.EXTRA_INSTALL_RESULT, PackageManager.INSTALL_SUCCEEDED);
             setResult(Activity.RESULT_OK, result);
             finish();
-        } else {
-            Intent intent = getIntent();
-            ApplicationInfo appInfo =
-                    intent.getParcelableExtra(PackageUtil.INTENT_ATTR_APPLICATION_INFO);
-            mAppPackageName = appInfo.packageName;
-            mAppSnippet = intent.getParcelableExtra(PackageInstallerActivity.EXTRA_APP_SNIPPET,
-                    PackageUtil.AppSnippet.class);
-
-            mLaunchIntent = getPackageManager().getLaunchIntentForPackage(mAppPackageName);
-
-            bindUi();
+            return;
         }
+
+        Intent intent = getIntent();
+        ApplicationInfo appInfo =
+                intent.getParcelableExtra(PackageUtil.INTENT_ATTR_APPLICATION_INFO);
+        if (appInfo == null) {
+            finish();
+            return;
+        }
+
+        mAppPackageName = appInfo.packageName;
+        mAppSnippet = intent.getParcelableExtra(
+                PackageInstallerActivity.EXTRA_APP_SNIPPET,
+                PackageUtil.AppSnippet.class
+        );
+
+        mLaunchIntent = getPackageManager()
+                .getLaunchIntentForPackage(mAppPackageName);
     }
 
     @Override
@@ -82,8 +70,29 @@ public class InstallSuccess extends Activity {
         bindUi();
     }
 
+    @Override
+    protected void onStop() {
+        dismissDialog();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        dismissDialog();
+        super.onDestroy();
+    }
+
+    private void dismissDialog() {
+        if (mDialog != null) {
+            if (mDialog.isShowing()) {
+                mDialog.dismiss();
+            }
+            mDialog = null;
+        }
+    }
+
     private void bindUi() {
-        if (mAppSnippet == null) {
+        if (mAppSnippet == null || mDialog != null) {
             return;
         }
 
@@ -91,40 +100,35 @@ public class InstallSuccess extends Activity {
         builder.setIcon(mAppSnippet.icon);
         builder.setTitle(mAppSnippet.label);
         builder.setView(R.layout.install_content_view);
-        builder.setPositiveButton(getString(R.string.launch), null);
-        builder.setNegativeButton(getString(R.string.done),
-                (ignored, ignored2) -> {
-                    if (mAppPackageName != null) {
-                        Log.i(LOG_TAG, "Finished installing " + mAppPackageName);
-                    }
-                    finish();
-                });
-        builder.setOnCancelListener(dialog -> {
-            if (mAppPackageName != null) {
-                Log.i(LOG_TAG, "Finished installing " + mAppPackageName);
-            }
-            finish();
-        });
+
+        builder.setNegativeButton(R.string.done, (d, w) -> finish());
+        builder.setOnCancelListener(d -> finish());
+
+        builder.setPositiveButton(R.string.launch, null);
+
         mDialog = builder.create();
         mDialog.show();
-        mDialog.requireViewById(R.id.install_success).setVisibility(View.VISIBLE);
-        // Show or hide "launch" button
-        boolean visible = false;
-        if (mLaunchIntent != null) {
-            List<ResolveInfo> list = getPackageManager().queryIntentActivities(mLaunchIntent,
-                    0);
-            if (list != null && list.size() > 0) {
-                visible = true;
-            }
-        }
-        visible = visible && isLauncherActivityEnabled(mLaunchIntent);
 
-        Button launchButton = mDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-        if (visible) {
-            launchButton.setOnClickListener(view -> {
+        mDialog.requireViewById(R.id.install_success)
+                .setVisibility(View.VISIBLE);
+
+        Button launchButton =
+                mDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+
+        boolean canLaunch = false;
+        if (mLaunchIntent != null) {
+            List<ResolveInfo> list =
+                    getPackageManager().queryIntentActivities(mLaunchIntent, 0);
+            canLaunch = list != null && !list.isEmpty()
+                    && isLauncherActivityEnabled(mLaunchIntent);
+        }
+
+        if (canLaunch) {
+            launchButton.setOnClickListener(v -> {
                 try {
                     startActivity(mLaunchIntent.addFlags(
-                        Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
+                            Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                    | Intent.FLAG_ACTIVITY_SINGLE_TOP));
                 } catch (ActivityNotFoundException | SecurityException e) {
                     Log.e(LOG_TAG, "Could not start activity", e);
                 }
@@ -133,13 +137,16 @@ public class InstallSuccess extends Activity {
         } else {
             launchButton.setVisibility(View.GONE);
         }
+
+        Log.i(LOG_TAG, "Finished installing " + mAppPackageName);
     }
 
     private boolean isLauncherActivityEnabled(Intent intent) {
         if (intent == null || intent.getComponent() == null) {
             return false;
         }
-        return getPackageManager().getComponentEnabledSetting(intent.getComponent())
-            != PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        return getPackageManager().getComponentEnabledSetting(
+                intent.getComponent())
+                != PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
     }
 }
