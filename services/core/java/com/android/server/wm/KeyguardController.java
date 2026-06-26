0e3e2c2bff85f2764fba0bc320d715e1cfd0e54c
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-26 14:45:12 +0800

    [feature] adding support for game space
    Change-Id: Iaaf20bcbaa50c42dd6b7739ae233b245a2c5ad5c

--- services/core/java/com/android/server/wm/KeyguardController.java
+++ services/core/java/com/android/server/wm/KeyguardController.java
@@ -61,6 +61,7 @@ import android.view.Display;
 import android.view.WindowManager;
 
 import com.android.internal.policy.IKeyguardDismissCallback;
+import com.android.server.LocalServices;
 import com.android.server.inputmethod.InputMethodManagerInternal;
 import com.android.server.policy.WindowManagerPolicy;
 import com.android.window.flags.Flags;
@@ -243,6 +244,14 @@ class KeyguardController {
         state.mKeyguardShowing = keyguardShowing;
         state.mAodShowing = aodShowing;
         state.writeEventLog("setKeyguardShown");
+
+        if (displayId == DEFAULT_DISPLAY && keyguardChanged) {
+            final GameSpaceService gameSpaceService =
+                    LocalServices.getService(GameSpaceService.class);
+            if (gameSpaceService != null) {
+                gameSpaceService.onKeyguardChanged(keyguardShowing);
+            }
+        }
 
         if (keyguardChanged || (mWindowManager.mFlags.mAodTransition && aodChanged)) {
             if (keyguardChanged) {
